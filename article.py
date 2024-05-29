51b9b6ca15cb08a5cfe6b76eb4796e32436994c3
sariya <dayzwaitik123@yandex.ru>
2024-05-30 00:16:58 +0300

    select

---
+++ article.py
@@ -0,0 +1,3 @@
+class Locators:
+    TITLES = "div.lenta-card > div.lenta-card-book > div.lenta-card-book__wrapper > a')"
+    
--- soup_page.py
+++ soup_page.py
@@ -8,19 +8,13 @@ class SoupPage:
         self.__soup_page = BeautifulSoup(self.__page.text, "html.parser")
     
     def get_all_reviews_book_titles(self):
-        titles = []
-        for i in self.__soup_page.select("article"):
-            titles.append(i.select('div.lenta-card > div.lenta-card-book > div.lenta-card-book__wrapper > a')[0].text.strip())
-        return titles
+        titles = self.__soup_page.select("div.lenta-card > div.lenta-card-book > div.lenta-card-book__wrapper > a")
+        return [t.text for t in titles]
 
     def get_all_reviews_book_likes(self):
-        likes = []
-        for i in self.__soup_page.select("article"):
-            likes.append(i.select('div.review-card__footer > div.sab > div[data-type="vote"] > a > span')[0].text.strip())
-        return likes
+        likes = self.__soup_page.select('div.review-card__footer > div.sab > div[data-type="vote"] > a > span')
+        return [l.text.strip() for l in likes]
 
     def get_all_reviews_book_watchs(self):
-        wathces = []
-        for i in self.__soup_page.select("article"):
-            wathces.append(i.select("div.lenta-card > .lenta-card__details > p:nth-child(2)")[0].text.strip())
-        return wathces
+        wathces = self.__soup_page.select("div.lenta-card > .lenta-card__details > p:nth-child(2)")
+        return [w.text.strip() for w in wathces]
