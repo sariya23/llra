e7a42fda5bcfd09070b8dd5847f4bb91a71bb120
sariya <dayzwaitik123@yandex.ru>
2024-05-30 00:33:56 +0300

    Add script for get all revs

--- soup_page.py
+++ soup_page.py
@@ -1,4 +1,5 @@
 from locatros import Locators
+from article import Article
 
 from bs4 import BeautifulSoup
 import requests
@@ -17,6 +18,18 @@ class SoupPage:
         likes = self.__soup_page.select(Locators.LIKES)
         return [l.text.strip() for l in likes]
 
-    def get_all_reviews_book_watchs(self):
+    def get_all_reviews_book_watches(self):
         wathces = self.__soup_page.select(Locators.WATCHES)
         return [w.text.strip() for w in wathces]
+
+    def get_all_reviews_from_page(self):
+        reviews: list[Article] = []
+        likes = self.get_all_reviews_book_likes()
+        watches = self.get_all_reviews_book_watches()
+        titles = self.get_all_reviews_book_titles()
+
+        for l, w, t in zip(likes, watches, titles):
+            reviews.append(Article(title=t, watches=w, likes=l))
+        
+        return reviews
+    
