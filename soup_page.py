4cf6ef1764cf744a00ab026937bac9edaa6c8934
sariya <dayzwaitik123@yandex.ru>
2024-05-30 00:35:54 +0300

    Private methods

--- soup_page.py
+++ soup_page.py
@@ -10,23 +10,23 @@ class SoupPage:
         self.__page= requests.get(url)
         self.__soup_page = BeautifulSoup(self.__page.text, "html.parser")
     
-    def get_all_reviews_book_titles(self):
+    def __get_all_reviews_book_titles(self):
         titles = self.__soup_page.select(Locators.TITLES)
         return [t.text for t in titles]
 
-    def get_all_reviews_book_likes(self):
+    def __get_all_reviews_book_likes(self):
         likes = self.__soup_page.select(Locators.LIKES)
         return [l.text.strip() for l in likes]
 
-    def get_all_reviews_book_watches(self):
+    def __get_all_reviews_book_watches(self):
         wathces = self.__soup_page.select(Locators.WATCHES)
         return [w.text.strip() for w in wathces]
 
     def get_all_reviews_from_page(self):
         reviews: list[Article] = []
-        likes = self.get_all_reviews_book_likes()
-        watches = self.get_all_reviews_book_watches()
-        titles = self.get_all_reviews_book_titles()
+        likes = self.__get_all_reviews_book_likes()
+        watches = self.__get_all_reviews_book_watches()
+        titles = self.__get_all_reviews_book_titles()
 
         for l, w, t in zip(likes, watches, titles):
             reviews.append(Article(title=t, watches=w, likes=l))
@@ -34,4 +34,4 @@ class SoupPage:
         return reviews
     
     def is_reviews_on_page(self) -> bool:
-        return bool(self.get_all_reviews_book_titles())
+        return bool(self.__get_all_reviews_book_titles())
