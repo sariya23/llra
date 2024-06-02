e68bc5441cb3d4d51a2cfe59e7b917001679d855
sariya <dayzwaitik123@yandex.ru>
2024-06-03 00:41:20 +0300

    Add str and rating

--- article.py
+++ article.py
@@ -2,9 +2,13 @@ from dataclasses import dataclass
 from datetime import datetime
 
 
-@dataclass(frozen=True, slots=True)
+@dataclass
 class Article:
     title: str
     likes: int
     watches: int
     publish_datetime: datetime
+    rating: float = 0.0
+
+    def __str__(self):
+        return f"Title: {self.title}; likes: {self.likes}; watches: {self.watches}; publish_date: {self.publish_datetime}, rating: {self.rating}"
