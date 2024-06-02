c30906ffdfae6e0d596312e6c7be0be4489568cd
sariya <dayzwaitik123@yandex.ru>
2024-06-03 00:41:46 +0300

    add method for calc rating

--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+
+from article import Article
+
+
 def convert_number_with_K_notations_to_int(amount_of_watches: str) -> int:
     """
     Переводит запись вида nK в целое число n.
@@ -14,3 +19,15 @@ def convert_number_with_K_notations_to_int(amount_of_watches: str) -> int:
         return number * 1000
 
     return int(amount_of_watches)
+
+
+def calculate_rating_of_review(review: Article) -> float:
+    """
+    Формула: likes/watches * 1/(curr_date - publish_date)
+
+    likes/wathes - STR
+    1/(curr_date - publish_date) - чем меньше будет отрезок времени, тем
+    больше будет значение
+    """
+    time_delta = datetime.today().toordinal() - review.publish_datetime.toordinal()
+    return ((review.likes / review.watches) * (1 / time_delta)) * 100
