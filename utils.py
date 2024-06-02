b22fe026f2742301260c16f17a2d7d9a086cd69a
sariya <dayzwaitik123@yandex.ru>
2024-06-03 00:53:06 +0300

    create func for get all reviews of user

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from review import Review
+from soup_page import SoupPage
 from variables import Variables
 
 
@@ -36,3 +37,16 @@ def calculate_rating_of_review(review: Review) -> float:
 
 def create_url_to_user_reviews(user_name: str) -> str:
     return f"{Variables.BASE_URL}{user_name}{Variables.REVIEW_POSTFIX}"
+
+
+def get_all_reviews_of_user(user_name: str) -> list[Review]:
+    i = 2
+    url = create_url_to_user_reviews(user_name)
+    page = SoupPage(url)
+    reviews = []
+    while page.is_reviews_on_page():
+        articles = page.get_all_reviews_from_page()
+        reviews.extend(articles)
+        page = SoupPage(f"{url}/~{i}")
+        i += 1
+    return reviews
