376fe17272d63f668e5df94187e535100db2ec6c
sariya <dayzwaitik123@yandex.ru>
2024-06-03 01:10:26 +0300

    No dataclass

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-from soup_page import SoupPage
-from utils import calculate_rating_of_review
+from utils import get_all_reviews_of_user
 
-BASE_URL = "https://www.livelib.ru/reader/sariya1/reviews/~1"
-page = SoupPage(BASE_URL)
-i = 2
-reviews = []
-while page.is_reviews_on_page():
-    articles = page.get_all_reviews_from_page()
-    reviews.extend(articles)
-    page = SoupPage(f"{BASE_URL}/~{i}")
-    i += 1
-
-for i in range(len(reviews)):
-    rating = calculate_rating_of_review(reviews[i])
-    reviews[i].rating = rating
-
-print(*reviews, sep="\n")
+reviews = get_all_reviews_of_user("sariya1")
+print(
+    *sorted(reviews, key=lambda r: r.calculate_rating_of_review(), reverse=True),
+    sep="\n"
+)
--- review.py
+++ review.py
@@ -1,14 +1,21 @@
-from dataclasses import dataclass
 from datetime import datetime
 
 
-@dataclass
 class Review:
-    title: str
-    likes: int
-    watches: int
-    publish_datetime: datetime
-    rating: float = 0.0
+    def __init__(
+        self, title: str, likes: int, watches: int, publish_datetime: datetime
+    ):
+        self.title = title
+        self.likes = likes
+        self.watches = watches
+        self.publish_datetime = publish_datetime
 
-    def __str__(self):
-        return f"Title: {self.title}; likes: {self.likes}; watches: {self.watches}; publish_date: {self.publish_datetime}, rating: {self.rating}"
+    def __repr__(self):
+        return f"Title: {self.title}; likes: {self.likes}; watches: {self.watches}; publish_date: {self.publish_datetime}; rating: {self.calculate_rating_of_review()}"
+
+    def calculate_rating_of_review(self):
+        """
+        Формула: likes/watches * 1/(curr_date - publish_date)
+        """
+        time_delta = datetime.today().toordinal() - self.publish_datetime.toordinal()
+        return ((self.likes / self.watches) * (1 / time_delta)) * 100
