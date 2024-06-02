7cfba768c46cd206a5bf503a3e9bb38638571123
sariya <dayzwaitik123@yandex.ru>
2024-06-02 23:04:30 +0300

    Add function to conver nK notation in number

---
+++ utils.py
@@ -0,0 +1,14 @@
+def convert_number_with_K_to_int(amount_of_watches: str) -> int:
+    """
+    Переводит запись вида nK в целое число n.
+
+    Например:
+    >>> convert_number_with_K_to_int("2K")
+    >>> 2000
+    """
+    if "K" in amount_of_watches:
+        k_index = amount_of_watches.find("K")
+        number = int(amount_of_watches[:k_index])
+        return number * 1000
+
+    return int(amount_of_watches)
