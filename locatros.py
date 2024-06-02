d776521fe50f66d10214f86b693101ec30779511
sariya <dayzwaitik123@yandex.ru>
2024-06-02 23:51:31 +0300

    Add datetime

--- locatros.py
+++ locatros.py
@@ -1,5 +1,5 @@
 class Locators:
-    TITLES = 'div.lenta-card > div.lenta-card-book > div.lenta-card-book__wrapper > a'
+    TITLES = "div.lenta-card > div.lenta-card-book > div.lenta-card-book__wrapper > a"
     LIKES = 'div.review-card__footer > div.sab > div[data-type="vote"] > a > span'
-    WATCHES = '.lenta-card__aliases'
-    
+    WATCHES = ".lenta-card__aliases"
+    PUBLISH_DATETIME = ".lenta-card__date"
