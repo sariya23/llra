49af98bd3c7b6c4fa5a2c057a101592cf7fe29e3
sariya <dayzwaitik123@yandex.ru>
2024-06-02 23:51:56 +0300

    Add method to translate russian month to eng

--- utils.py
+++ utils.py
@@ -14,3 +14,25 @@ def convert_number_with_K_notations_to_int(amount_of_watches: str) -> int:
         return number * 1000
 
     return int(amount_of_watches)
+
+
+def russian_month_name_to_english_with_title(russian_month_name: str) -> str:
+    """
+    Переводит название месяца с русского языка на английский и переводит
+    первую букву месяца в верхний регистр.
+    """
+    ru_en_month = {
+        "января": "Junuary",
+        "февраля": "February",
+        "марта": "March",
+        "апреля": "April",
+        "мая": "May",
+        "июня": "June",
+        "июля": "July",
+        "августа": "August",
+        "сентября": "September",
+        "октября": "October",
+        "ноября": "November",
+        "декабря": "December",
+    }
+    return ru_en_month[russian_month_name]
