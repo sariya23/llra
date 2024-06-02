0084b252713cf5fd77a491c69a7c6b9e3abadad4
sariya <dayzwaitik123@yandex.ru>
2024-06-03 01:10:53 +0300

    Move methods

--- utils.py
+++ utils.py
@@ -1,38 +1,6 @@
-from datetime import datetime
-
 from review import Review
 from soup_page import SoupPage
 from variables import Variables
-
-
-def convert_number_with_K_notations_to_int(amount_of_watches: str) -> int:
-    """
-    Переводит запись вида nK в целое число n.
-
-    Например:
-    >>> convert_number_with_K_notations_to_int("2K")
-    >>> 2000
-
-    !K Латинская!
-    """
-    if "K" in amount_of_watches:
-        k_index = amount_of_watches.find("K")
-        number = int(amount_of_watches[:k_index])
-        return number * 1000
-
-    return int(amount_of_watches)
-
-
-def calculate_rating_of_review(review: Review) -> float:
-    """
-    Формула: likes/watches * 1/(curr_date - publish_date)
-
-    likes/wathes - STR
-    1/(curr_date - publish_date) - чем меньше будет отрезок времени, тем
-    больше будет значение
-    """
-    time_delta = datetime.today().toordinal() - review.publish_datetime.toordinal()
-    return ((review.likes / review.watches) * (1 / time_delta)) * 100
 
 
 def create_url_to_user_reviews(user_name: str) -> str:
