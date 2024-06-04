cd2f35db05fc9f07e4c15dd0fa758f33837061a9
sariya <dayzwaitik123@yandex.ru>
2024-06-04 21:32:34 +0300

    Positive test for k not

---
+++ tests/soup_page_test.py
@@ -0,0 +1,12 @@
+import pytest
+
+from soup_page import SoupPage
+
+
+@pytest.mark.parametrize(
+    "value,expected_value", [("123", 123), ("1K", 1000), ("10K", 10_000), ("999", 999)]
+)
+def test_convert_number_with_K_notations_to_int_positive(
+    value: str, expected_value: int
+):
+    assert SoupPage.convert_number_with_K_notations_to_int(value) == expected_value
