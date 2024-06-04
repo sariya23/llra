c16552249a831994298384bdb086cde6e22ddc15
sariya <dayzwaitik123@yandex.ru>
2024-06-04 21:18:15 +0300

    Add positive test for date parser

--- tests/date_parser_test.py
+++ tests/date_parser_test.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Any
 
 import pytest
@@ -38,3 +39,12 @@ def test_translate_ru_month_to_en_month_neagtive(wrong_ru_month_name: Any):
         DateParser._russian_month_name_to_english_and_do_nominative_case(
             wrong_ru_month_name
         )
+
+
+@pytest.mark.parametrize(
+    "datetime_str,expected_object_datetime",
+    [("18\xa0апреля\xa02024\xa0г. 16:52", datetime(2024, 4, 18, hour=16, minute=52))],
+)
+def test_parse_date_positive(datetime_str: str, expected_object_datetime: datetime):
+    parser = DateParser(datetime_str)
+    assert parser.parse_date() == expected_object_datetime
