8c4028cfafcef0d11026b78747370f1d11281b86
sariya <dayzwaitik123@yandex.ru>
2024-06-04 22:16:15 +0300

    Add expcs

--- src/exceptions.py
+++ src/exceptions.py
@@ -8,3 +8,15 @@ class WrongDatetimeFormatFromApp(LandingException):
 
 class WrongMonthCaseFromApp(LandingException):
     pass
+
+
+class ArgsException(Exception):
+    pass
+
+
+class NoArguments(ArgsException):
+    pass
+
+
+class MissRequiredArgument(ArgsException):
+    pass
