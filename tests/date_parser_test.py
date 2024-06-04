aebeeecf6fd03147841666fc01650870ab1705d5
sariya <dayzwaitik123@yandex.ru>
2024-06-04 21:28:19 +0300

    Add negative test case

--- tests/date_parser_test.py
+++ tests/date_parser_test.py
@@ -4,7 +4,7 @@ from typing import Any
 import pytest
 
 from date_parser import DateParser
-from exceptions import WrongDatetimeFormatFromApp
+from exceptions import WrongDatetimeFormatFromApp, WrongMonthCaseFromApp
 
 
 @pytest.mark.parametrize(
@@ -35,7 +35,7 @@ def test_translate_ru_month_to_en_month_positive(
 
 @pytest.mark.parametrize("wrong_ru_month_name", ("йцу", 123, (1, 2, 3)))
 def test_translate_ru_month_to_en_month_neagtive(wrong_ru_month_name: Any):
-    with pytest.raises(WrongDatetimeFormatFromApp):
+    with pytest.raises(WrongMonthCaseFromApp):
         DateParser._russian_month_name_to_english_and_do_nominative_case(
             wrong_ru_month_name
         )
@@ -48,3 +48,10 @@ def test_translate_ru_month_to_en_month_neagtive(wrong_ru_month_name: Any):
 def test_parse_date_positive(datetime_str: str, expected_object_datetime: datetime):
     parser = DateParser(datetime_str)
     assert parser.parse_date() == expected_object_datetime
+
+
+@pytest.mark.parametrize("wrong_datetime_format", ("18мая 2024 г. 12:45", "uewfiwef"))
+def test_parse_date_negative(wrong_datetime_format: str):
+    with pytest.raises(WrongDatetimeFormatFromApp):
+        parser = DateParser(wrong_datetime_format)
+        parser.parse_date()
