c8b378ff19c5ef33471ae4bb922ba0368232e7c8
sariya <dayzwaitik123@yandex.ru>
2024-06-04 22:26:10 +0300

    file write

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ if __name__ == "__main__":
     args = arg_parser.set_value_of_args_true_false_default()
 
     reviews = get_all_reviews_of_user(args["username"])
+    reviews = sorted(
+        reviews,
+        key=lambda r: r.calculate_rating_of_review(),
+        reverse=args["reverse"],
+    )
 
-    if not args["in_file"]:
-        for review in sorted(
-            reviews,
-            key=lambda r: r.calculate_rating_of_review(),
-            reverse=args["reverse"],
-        ):
-            print(review)
+    if args["in_file"]:
+        with open("res.txt", "w", encoding="utf-8") as f:
+            f.writelines([f"{str(r)}\n" for r in reviews])
+    else:
+        print(*reviews, sep='\n')
