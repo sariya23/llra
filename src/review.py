c9d050d3738c4b89f4b290800620dd85e3894121
sariya <dayzwaitik123@yandex.ru>
2024-06-08 14:08:20 +0300

    Dont show rating

--- src/review.py
+++ src/review.py
@@ -15,9 +15,7 @@ class Review:
         return f"Review({self.title}, {self.likes}, {self.watches}, {self.publish_datetime}, {self.calculate_rating_of_review()})"
 
     def __str__(self):
-        return (
-            f"Название книг: {self.title}; рейтинг: {self.calculate_rating_of_review()}"
-        )
+        return f"Название книг: {self.title}"
 
     def calculate_rating_of_review(self):
         """
