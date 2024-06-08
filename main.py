46f362febf3ead5a467455e76372b326617b3f74
sariya <dayzwaitik123@yandex.ru>
2024-06-08 13:21:29 +0300

    Use arg parser for parse args

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-import sys
+import argparse
 
-from src.arg_parser import ArgParser
 from src.utils import get_all_reviews_of_user
 
 if __name__ == "__main__":
-    arg_parser = ArgParser(sys.argv[1:])
-    args = arg_parser.set_value_of_args_true_false_default()
-
-    reviews = get_all_reviews_of_user(args["username"])
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument(
+        "--username", type=str, help="Required user name", required=True
+    )
+    arg_parser.add_argument(
+        "--in_file", action="store_true", help="Write result in file"
+    )
+    arg_parser.add_argument(
+        "--asc_sort",
+        action="store_false",
+        help="The order of sort is ascending. Deafult is descending sort",
+    )
+    args = arg_parser.parse_args()
+    reviews = get_all_reviews_of_user()
     reviews = sorted(
         reviews,
         key=lambda r: r.calculate_rating_of_review(),
@@ -18,4 +27,4 @@ if __name__ == "__main__":
         with open("res.txt", "w", encoding="utf-8") as f:
             f.writelines([f"{str(r)}\n" for r in reviews])
     else:
-        print(*reviews, sep='\n')
+        print(*reviews, sep="\n")
